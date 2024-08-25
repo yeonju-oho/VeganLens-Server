5a9136300ea513f38f9c230d1792a20a429076c1
yeonju-oho <yeonju.oho@gmail.com>
2024-08-26 05:56:31 +0900

    mongodb Atlas로 변경

--- db/index.js
+++ db/index.js
@@ -1,8 +1,11 @@
 // db/index.js
-
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/nicknameDB');
+// MongoDB Atlas 연결 문자열로 수정
+const uri = "mongodb+srv://yeonjuoho:yjPJyZomRO6xAZud@vegenlens.lo4mt.mongodb.net/veganLens?retryWrites=true&w=majority";
+
+// Mongoose를 사용하여 MongoDB Atlas에 연결
+mongoose.connect(uri);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
